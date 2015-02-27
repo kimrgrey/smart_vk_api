072102573035e1efd8083d1facd4c6f4387f89d6
Sergey Tsvetkov <sergey.a.tsvetkov@gmail.com>
2015-02-28 01:25:16 +0300

    Used users.get instead of scope.method in spec. Just to be more realistic.

--- spec/call_spec.rb
+++ spec/call_spec.rb
@@ -18,27 +18,27 @@ describe SmartVkApi::VK do
     end
     
     it 'should take care of method name' do
-      expect(vk.method_url('scope.method')).to eq('https://api.vk.com/method/scope.method')
+      expect(vk.method_url('users.get')).to eq('https://api.vk.com/method/users.get')
     end
 
     it 'should take care of parameters' do
-      expect(vk.method_url('scope.method', {:aaa => :bbb, :xxx => :yyy})).to eq('https://api.vk.com/method/scope.method?aaa=bbb&xxx=yyy')
+      expect(vk.method_url('users.get', {:aaa => :bbb, :user_ids => :kimrgrey})).to eq('https://api.vk.com/method/users.get?aaa=bbb&user_ids=kimrgrey')
     end
   end
 
   describe '#http' do
     it 'should return http connection if block was not given' do
-      expect( vk.http('scope.method')).to be_a(Net::HTTP)
+      expect( vk.http('users.get')).to be_a(Net::HTTP)
     end
     
     it 'should return result of block call if block was given' do
-      stub_request(:get, "https://api.vk.com/method/scope.method").to_return(:status => 200, :body => default_response_body.to_json)
-      expect( vk.http('scope.method') { |_| 'result of block' }).to eq('result of block')
+      stub_request(:get, "https://api.vk.com/method/users.get").to_return(:status => 200, :body => default_response_body.to_json)
+      expect( vk.http('users.get') { |_| 'result of block' }).to eq('result of block')
     end
 
     it 'should pass http connection as parameter into block' do
-      stub_request(:get, "https://api.vk.com/method/scope.method").to_return(:status => 200, :body => default_response_body.to_json)
-      expect(vk.http('scope.method') { |http| http }).to be_a(Net::HTTPResponse)
+      stub_request(:get, "https://api.vk.com/method/users.get").to_return(:status => 200, :body => default_response_body.to_json)
+      expect(vk.http('users.get') { |http| http }).to be_a(Net::HTTPResponse)
     end
 
     it 'should setup appropriate headers' do
@@ -46,54 +46,54 @@ describe SmartVkApi::VK do
         'Content-Type' =>'application/json',
         'Content-Type' =>'application/json'
       }
-      stub_request(:get, "https://api.vk.com/method/scope.method").to_return(:status => 200, :body => default_response_body.to_json)
-      vk.call('scope.method')
-      expect(WebMock).to have_requested(:get, "https://api.vk.com/method/scope.method").with(:headers => headers)
+      stub_request(:get, "https://api.vk.com/method/users.get").to_return(:status => 200, :body => default_response_body.to_json)
+      vk.call('users.get')
+      expect(WebMock).to have_requested(:get, "https://api.vk.com/method/users.get").with(:headers => headers)
     end
   end
 
   describe '#call' do
     it 'should send request to appropriate url according to method name' do
-      stub_request(:get, "https://api.vk.com/method/scope.method").to_return(:status => 200, :body => default_response_body.to_json)
-      vk.call('scope.method')
-      expect(WebMock).to have_requested(:get, "https://api.vk.com/method/scope.method")
+      stub_request(:get, "https://api.vk.com/method/users.get").to_return(:status => 200, :body => default_response_body.to_json)
+      vk.call('users.get')
+      expect(WebMock).to have_requested(:get, "https://api.vk.com/method/users.get")
     end
 
     it 'should send request to appropriate url according params' do
-      stub_request(:get, "https://api.vk.com/method/scope.method?xxx=yyy").to_return(:status => 200, :body => default_response_body.to_json)
-      vk.call('scope.method', {:xxx => 'yyy'})
-      expect(WebMock).to have_requested(:get, "https://api.vk.com/method/scope.method?xxx=yyy")
+      stub_request(:get, "https://api.vk.com/method/users.get?user_ids=kimrgrey").to_return(:status => 200, :body => default_response_body.to_json)
+      vk.call('users.get', {:user_ids => 'kimrgrey'})
+      expect(WebMock).to have_requested(:get, "https://api.vk.com/method/users.get?user_ids=kimrgrey")
     end
 
     it 'should return raise MethodCallError if body of response is nil' do
-      stub_request(:get, "https://api.vk.com/method/scope.method").to_return(:status => 200, :body => nil)
-      expect { vk.call('scope.method') }.to raise_error(SmartVkApi::MethodCallError, 'Response could not be empty')
+      stub_request(:get, "https://api.vk.com/method/users.get").to_return(:status => 200, :body => nil)
+      expect { vk.call('users.get') }.to raise_error(SmartVkApi::MethodCallError, 'Response could not be empty')
     end
 
     it 'should return raise MethodCallError if body of response is empty string' do
-      stub_request(:get, "https://api.vk.com/method/scope.method").to_return(:status => 200, :body => "")
-      expect { vk.call('scope.method') }.to raise_error(SmartVkApi::MethodCallError, 'Response could not be empty')
+      stub_request(:get, "https://api.vk.com/method/users.get").to_return(:status => 200, :body => "")
+      expect { vk.call('users.get') }.to raise_error(SmartVkApi::MethodCallError, 'Response could not be empty')
     end
 
     it 'should return raise MethodCallError if body of response has no key :response' do
-      stub_request(:get, "https://api.vk.com/method/scope.method").to_return(:status => 200, :body => { :bad => :response }.to_json)
-      expect { vk.call('scope.method') }.to raise_error(SmartVkApi::MethodCallError, 'Response should include key named :response')
+      stub_request(:get, "https://api.vk.com/method/users.get").to_return(:status => 200, :body => { :bad => :response }.to_json)
+      expect { vk.call('users.get') }.to raise_error(SmartVkApi::MethodCallError, 'Response should include key named :response')
     end
 
     it 'should raise MethodCallError error in case of internal server error' do
-      stub_request(:get, "https://api.vk.com/method/scope.method").to_return(:status => 500, :body => 'Internal Server Error')
-      expect { vk.call('scope.method') }.to raise_error(SmartVkApi::MethodCallError, 'Internal Server Error')
+      stub_request(:get, "https://api.vk.com/method/users.get").to_return(:status => 500, :body => 'Internal Server Error')
+      expect { vk.call('users.get') }.to raise_error(SmartVkApi::MethodCallError, 'Internal Server Error')
     end
 
     it 'should raise MethodCallError in case of method call error' do
       response = { :error => { :error_code => 113, :error_msg => 'Invalid user id' } }
-      stub_request(:get, "https://api.vk.com/method/scope.method").to_return(:status => 200, :body => response.to_json)
-      expect { vk.call('scope.method') }.to raise_error(SmartVkApi::MethodCallError, response.to_json)
+      stub_request(:get, "https://api.vk.com/method/users.get").to_return(:status => 200, :body => response.to_json)
+      expect { vk.call('users.get') }.to raise_error(SmartVkApi::MethodCallError, response.to_json)
     end
 
     it 'should return value of :response key of returned json' do
-      stub_request(:get, "https://api.vk.com/method/scope.method").to_return(:status => 200, :body => default_response_body.to_json)
-      expect(vk.call('scope.method')).to eq(default_response_body[:response])
+      stub_request(:get, "https://api.vk.com/method/users.get").to_return(:status => 200, :body => default_response_body.to_json)
+      expect(vk.call('users.get')).to eq(default_response_body[:response])
     end
   end
 end
